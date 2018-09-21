955c2b8cb5ef33225b5eed0c24c30ae53f3dbfc8
christianfrom <43375010+christianfrom@users.noreply.github.com>
2018-09-21 09:20:21 +0200

    Initial commit

---
+++ HvilketStørst/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HvilketStørst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv to tal, så vil jeg fortælle, hvilket et er størst");
+            Console.WriteLine("Det første tal");
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Det andet tal");
+            int tal2 = Convert.ToInt32(Console.ReadLine());
+
+            sammenlign(tal1, tal2);
+
+    
+            Console.ReadKey();
+        }
+
+        private static void sammenlign(int tal1, int tal2)
+        {
+            if (tal1 > tal2)
+            {
+                Console.WriteLine("Det første tal er størst!");               
+            }
+
+            if (tal1 < tal2)
+            {
+                Console.WriteLine("Det andet tal er størst!");               
+            }
+
+            if (tal1 == tal2)
+            {
+                Console.WriteLine("De er lige store!");               
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# HvilketSt-rst
